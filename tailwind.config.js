4bd37e0f069db7f66afe5a5b473963d05c3da33d
Grant Sivley <grant.sivley@gmail.com>
2022-03-27 23:06:19 -0700

    initial react app

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "matt-primary-100": "#e3e8e6",
+        "matt-primary-300": "#c7d1cc",
+        "matt-primary-500": "#062C30",
+        "matt-primary-700": "#9dafa6",
+        "matt-primary-800": "#81988d",
+        "matt-primary-900": "#5c7067",
+
+        "matt-secondary-100": "#e3e8e6",
+        "matt-secondary-300": "#c7d1cc",
+        "matt-secondary-500": "#05595B",
+        "matt-secondary-700": "#9dafa6",
+        "matt-secondary-800": "#81988d",
+        "matt-secondary-900": "#5c7067", 
+        
+        "matt-accent-100": "#e3e8e6",
+        "matt-accent-300": "#c7d1cc",
+        "matt-accent-500": "#E2D784",
+        "matt-accent-700": "#9dafa6",
+        "matt-accent-800": "#81988d",
+        "matt-accent-900": "#5c7067", 
+
+        "matt-bg-100": "#e3e8e6",
+        "matt-bg-300": "#c7d1cc",
+        "matt-bg-500": "#F5F5F5",
+        "matt-bg-700": "#9dafa6",
+        "matt-bg-800": "#81988d",
+        "matt-bg-900": "#5c7067", 
+      },
+    },
+  },
+  plugins: [],
+}
